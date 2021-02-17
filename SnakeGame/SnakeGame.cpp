bd5d8eb65604e816ecfb8ded3d16ed0c1ea1a4ec
koendamme <koen.damme1999@gmail.com>
2021-02-17 16:19:29 +0100

    Game is now over when the snake hits its own tail

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -23,6 +23,7 @@ int main()
     pair<int, int> moveDelta = { 1, 0 };
 
     while (!gameOver) {
+        // Handle movement
         if (_kbhit()) {
             char input = _getch();
             if (input == 'a') {
@@ -41,27 +42,42 @@ int main()
 
         snake.Move(moveDelta);
 
+        // Make snake jump to other side of the map when it hits the wall
         pair<int, int> currentHeadPosition = snake.snakePositions.front();
         if (currentHeadPosition.first < 0) {
-            snake.SetPosition({ FIELD_WIDTH, currentHeadPosition.second });
+            snake.SetPosition({ FIELD_WIDTH - 1, currentHeadPosition.second });
         }
-        if (currentHeadPosition.first > FIELD_WIDTH) {
+        if (currentHeadPosition.first > FIELD_WIDTH - 1) {
             snake.SetPosition({ 0, currentHeadPosition.second });
         }
         if (currentHeadPosition.second < 0) {
-            snake.SetPosition({ currentHeadPosition.first, FIELD_HEIGHT });
+            snake.SetPosition({ currentHeadPosition.first, FIELD_HEIGHT - 1 });
         }
-        if (currentHeadPosition.second > FIELD_HEIGHT) {
+        if (currentHeadPosition.second > FIELD_HEIGHT - 1) {
             snake.SetPosition({ currentHeadPosition.first, 0 });
         }
 
-        game.DisplayField();
-        
+        // Check if the snake hit a candy
         if (snake.snakePositions.front().first == game.GetCandyPosition().first && snake.snakePositions.front().second == game.GetCandyPosition().second) {
             snake.PickUpCandy();
             game.SpawnCandy();
         }
 
+        // Check if the snake hit its tale
+        list<pair<int, int>> currentSnakePositions = snake.snakePositions;
+        if (currentSnakePositions.size() > 1) {
+            pair<int, int> headPosition = currentSnakePositions.front();
+            currentSnakePositions.pop_front();
+
+            for (pair<int, int> tailPosition : currentSnakePositions) {
+                if (headPosition.first == tailPosition.first && headPosition.second == tailPosition.second) {
+                    gameOver = true;
+                }
+            }
+        }
+
+        // Update the game field
+        game.DisplayField();
         Sleep(200);
     }
 }
